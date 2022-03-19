19a502f5f8ce45df2a5777e20bf9951d9beff3cc
vltanh <narubees@gmail.com>
2022-03-19 13:35:29 +0000

    train detection in multiple ways (instance seg or detection)

--- train_detect.py
+++ train_detect.py
@@ -3,25 +3,34 @@ from detectron2.config import get_cfg
 from detectron2.data.datasets import register_coco_instances
 
 import os
+import sys
+
+METHOD = sys.argv[1]
 
 register_coco_instances("chicken", {},
                         "list/train.json", "data/Dataset/train/img")
 
 cfg = get_cfg()
-# cfg.merge_from_file(
-#     "./detectron2_repo/configs/COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"
-# )
-cfg.merge_from_file(
-    "detectron2_repo/configs/COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml"
-)
+if METHOD == 'ins':
+    cfg.merge_from_file(
+        "./detectron2_repo/configs/COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"
+    )
+elif METHOD == 'det':
+    cfg.merge_from_file(
+        "detectron2_repo/configs/COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml"
+    )
+else:
+    sys.exit()
 
 cfg.DATASETS.TRAIN = ("chicken",)
 cfg.DATASETS.TEST = ()  # no metrics implemented for this dataset
 cfg.DATALOADER.NUM_WORKERS = 4
 
 # initialize from model zoo
-# cfg.MODEL.WEIGHTS = "detectron2://COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x/137849600/model_final_f10217.pkl"
-cfg.MODEL.WEIGHTS = "detectron2://COCO-Detection/faster_rcnn_R_50_FPN_3x/137849458/model_final_280758.pkl"
+if METHOD == 'ins':
+    cfg.MODEL.WEIGHTS = "detectron2://COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x/137849600/model_final_f10217.pkl"
+elif METHOD == 'det':
+    cfg.MODEL.WEIGHTS = "detectron2://COCO-Detection/faster_rcnn_R_50_FPN_3x/137849458/model_final_280758.pkl"
 
 cfg.SOLVER.IMS_PER_BATCH = 12
 cfg.SOLVER.BASE_LR = 0.02
